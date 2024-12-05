25e821689f2ac16b0abb4da30f025d472042b6a7
Elliott Saltar <esaltar@gmail.com>
2024-12-05 14:02:11 -0600

    homepage post box (not functional yet)

--- js/homepage.js
+++ js/homepage.js
@@ -1,7 +1,13 @@
 import { setTimeline, Timelines, renderTimeline } from "./masto_ts.js";
 import { getInclude } from "./modules/includes.mjs";
+const postInput = document.getElementById("post-input");
 document.getElementById("load-more-button").addEventListener("click", () => {
     renderTimeline();
+});
+postInput.style.height = `${postInput.scrollHeight}px`;
+postInput.addEventListener("input", (event) => {
+    event.target.style.height = "auto";
+    event.target.style.height = `${event.target.scrollHeight}px`;
 });
 setTimeline(Timelines.Home);
 getInclude(new URL("/include/navbar.html", window.location.origin)).then((include) => {
--- js/homepage.ts
+++ js/homepage.ts
@@ -1,8 +1,17 @@
 import { setTimeline, Timelines, renderTimeline } from "./masto_ts.js";
 import { getInclude } from "./modules/includes.mjs";
 
+const postInput: HTMLElement = document.getElementById("post-input");
+
 document.getElementById("load-more-button").addEventListener("click", () => {
 	renderTimeline();
+});
+
+postInput.style.height = `${postInput.scrollHeight}px`;
+
+postInput.addEventListener("input", (event) => {
+	(event.target as HTMLInputElement).style.height = "auto";
+	(event.target as HTMLInputElement).style.height = `${(event.target as HTMLInputElement).scrollHeight}px`;
 });
 
 setTimeline(Timelines.Home);
